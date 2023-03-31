0ce8dd2c8a485d6e38ad469445d5597924eedbe6
Noor Rahmi <95263271+rahmi1016@users.noreply.github.com>
2023-03-31 09:39:05 +0800

    Update script.js
    Fixing the read messagges portion

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-var hasRead = document.querySelectorAll(".read").length;
-var boxNumber = document.querySelectorAll(".box").length;
-document.querySelector(".number").innerHTML = boxNumber;
-var defaultNum = boxNumber - hasRead;
-
-function handleRead(element) {
-  element.classList.add("read");
-  var dots = element.querySelector("span.dot");
-  dots.classList.remove("dot");
-  defaultNum -= 1;
+window.onload = function notification() {
+  var hasRead = document.querySelectorAll(".read").length;
+  var boxNumber = document.querySelectorAll(".box").length;
+  document.querySelector(".number").innerHTML = boxNumber;
+  var defaultNum = boxNumber - hasRead;
   document.querySelector(".number").innerHTML = defaultNum;
-}
+};
 
 function readAll() {
   var boxes = document.querySelectorAll(".box");
@@ -17,4 +12,14 @@ function readAll() {
   var dots = document.querySelectorAll("span.dot");
   dots.forEach((dot) => dot.classList.remove("dot"));
   document.querySelector(".number").innerHTML = 0;
+}
+
+function handleRead(element) {
+  element.classList.add("read");
+  var hasRead = document.querySelectorAll(".read").length;
+  var boxNumber = document.querySelectorAll(".box").length;
+  document.querySelector(".number").innerHTML = boxNumber;
+  var defaultNum = boxNumber - hasRead + 1;
+  defaultNum -= 1;
+  document.querySelector(".number").innerHTML = defaultNum;
 }
